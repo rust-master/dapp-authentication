189c05e2cf39c6ffb87790a13d64aa40740af062
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-11 11:43:50 +0500

    isUserLoggedIn

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -7,20 +7,39 @@ contract AuthContract {
         address addr;
         string name;
         string password;
-        uint balance;
+        uint256 balance;
         string CNIC;
+        bool isUserLoggedIn;
     }
-
 
     mapping(string => UserDetail) user;
 
     // user registration function
-    function register(address _address, string memory _name, string memory _password, uint _balance, string memory _cnic) public returns (bool) {
+    function register(
+        address _address,
+        string memory _name,
+        string memory _password,
+        uint256 _balance,
+        string memory _cnic
+    ) public returns (bool) {
         user[_cnic].addr = _address;
         user[_cnic].name = _name;
         user[_cnic].password = _password;
         user[_cnic].balance = _balance;
         user[_cnic].CNIC = _cnic;
         return true;
+    }
+
+    function login(string memory _cnic, string memory _password)
+        public
+        returns (bool)
+    {
+        if (keccak256(abi.encodePacked(user[_cnic].password)) == keccak256(abi.encodePacked(_password))) {
+             user[_cnic].isUserLoggedIn = true;
+            return true;
+        } else {
+             user[_cnic].isUserLoggedIn = false;
+            return false;
+        }
     }
 }
