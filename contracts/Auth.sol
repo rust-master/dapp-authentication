5d700ec011fdd65c2010c5386dd5dc578ed7fb31
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-13 10:07:42 +0500

    formatting

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -1,58 +1,58 @@
-pragma solidity >=0.4.0 <0.6.0;
+    pragma solidity >=0.4.0 <0.6.0;
 
-contract Auth {
-    struct UserDetail {
-        address addr;
-        string name;
-        string password;
-        string balance;
-        string CNIC;
-        bool isUserLoggedIn;
-    }
+    contract Auth {
+        struct UserDetail {
+            address addr;
+            string name;
+            string password;
+            string balance;
+            string CNIC;
+            bool isUserLoggedIn;
+        }
 
-    mapping(address => UserDetail) user;
+        mapping(address => UserDetail) user;
 
-    // user registration function
-    function register(
-        address _address,
-        string memory _name,
-        string memory _password,
-        string memory _balance,
-        string memory _cnic
-    ) public returns (bool) {
-        require(user[_address].addr != msg.sender);
-        user[_address].addr = _address;
-        user[_address].name = _name;
-        user[_address].password = _password;
-        user[_address].balance = _balance;
-        user[_address].CNIC = _cnic;
-        user[_address].isUserLoggedIn = false;
-        return true;
-    }
+        // user registration function
+        function register(
+            address _address,
+            string memory _name,
+            string memory _password,
+            string memory _balance,
+            string memory _cnic
+        ) public returns (bool) {
+            require(user[_address].addr != msg.sender);
+            user[_address].addr = _address;
+            user[_address].name = _name;
+            user[_address].password = _password;
+            user[_address].balance = _balance;
+            user[_address].CNIC = _cnic;
+            user[_address].isUserLoggedIn = false;
+            return true;
+        }
 
-    // user login function
-    function login(address _address, string memory _password)
-        public
-        returns (bool)
-    {
-        if (
-            keccak256(abi.encodePacked(user[_address].password)) ==
-            keccak256(abi.encodePacked(_password))
-        ) {
-            user[_address].isUserLoggedIn = true;
-            return user[_address].isUserLoggedIn;
-        } else {
-            return false;
+        // user login function
+        function login(address _address, string memory _password)
+            public
+            returns (bool)
+        {
+            if (
+                keccak256(abi.encodePacked(user[_address].password)) ==
+                keccak256(abi.encodePacked(_password))
+            ) {
+                user[_address].isUserLoggedIn = true;
+                return user[_address].isUserLoggedIn;
+            } else {
+                return false;
+            }
+        }
+
+        // check the user logged In or not
+        function checkIsUserLogged(address _address) public view returns (bool) {
+            return (user[_address].isUserLoggedIn);
+        }
+
+        // logout the user
+        function logout(address _address) public {
+            user[_address].isUserLoggedIn = false;
         }
     }
-
-    // check the user logged In or not
-    function checkIsUserLogged(address _address) public view returns (bool) {
-        return (user[_address].isUserLoggedIn);
-    }
-
-    // logout the user
-    function logout(address _address) public {
-        user[_address].isUserLoggedIn = false;
-    }
-}
