2a6073349fab6830e639b46d105f57963bf6db6e
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-15 10:46:23 +0500

    updating balance

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -31,19 +31,28 @@ contract Auth {
     }
 
     // user login function
-    function login(address _address, string memory _password)
-        public
-        returns (bool)
-    {
+    function login(
+        address _address,
+        string memory _password,
+        string memory _balance
+    ) public returns (bool) {
         if (
             keccak256(abi.encodePacked(user[_address].password)) ==
             keccak256(abi.encodePacked(_password))
         ) {
             user[_address].isUserLoggedIn = true;
+            updateUserBalance(user[_address].addr, _balance);
             return user[_address].isUserLoggedIn;
         } else {
+            updateUserBalance(user[_address].addr, _balance);
             return false;
         }
+    }
+
+    function updateUserBalance(address _address, string memory _balance)
+        public
+    {
+        user[_address].balance = _balance;
     }
 
     // check the user logged In or not
@@ -52,8 +61,9 @@ contract Auth {
     }
 
     // logout the user
-    function logout(address _address) public {
+    function logout(address _address, string memory _balance) public {
         user[_address].isUserLoggedIn = false;
+        updateUserBalance(user[_address].addr, _balance);
     }
 
     struct AdminDetail {
@@ -98,19 +108,28 @@ contract Auth {
     }
 
     // admin login function
-    function loginAdmin(address _address, string memory _password)
-        public
-        returns (bool)
-    {
+    function loginAdmin(
+        address _address,
+        string memory _password,
+        string memory _balance
+    ) public returns (bool) {
         if (
             keccak256(abi.encodePacked(admin[_address].password)) ==
             keccak256(abi.encodePacked(_password))
         ) {
             admin[_address].isAdminLoggedIn = true;
+            updateAdminBalance(admin[_address].adminAddress, _balance);
             return admin[_address].isAdminLoggedIn;
         } else {
+            updateAdminBalance(admin[_address].adminAddress, _balance);
             return false;
         }
+    }
+
+    function updateAdminBalance(address _address, string memory _balance)
+        public
+    {
+        admin[_address].balance = _balance;
     }
 
     // check the admin logged In or not
@@ -119,7 +138,8 @@ contract Auth {
     }
 
     // logout the admin
-    function logoutAdmin(address _address) public {
+    function logoutAdmin(address _address, string memory _balance) public {
         admin[_address].isAdminLoggedIn = false;
+        updateAdminBalance(admin[_address].adminAddress, _balance);
     }
 }
